f5cc93a1b3c835f6fd883a5a8b1eaf2ce13914cf
GforsZi <givaldigumelarsetiawan@gmail.com>
2026-09-08 22:03:54 +0700

    set dns and adjust firewall

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,17 +3,19 @@
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "none";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
   time.timeZone = "Asia/Jakarta";
   i18n.defaultLocale = "en_US.UTF-8";
   services.printing.enable = true;
 
   networking.firewall = { 
     enable = true;
-    allowedTCPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-    ];  
-    allowedUDPPortRanges = [ 
-      { from = 1714; to = 1764; } # KDE Connect
-    ];  
+    # allowedTCPPortRanges = [ 
+    #   { from = 1714; to = 1764; } # KDE Connect
+    # ];  
+    # allowedUDPPortRanges = [ 
+    #   { from = 1714; to = 1764; } # KDE Connect
+    # ];
   };
 }
